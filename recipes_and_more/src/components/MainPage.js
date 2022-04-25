8e1e60a5a59309422b4c558315d4b3c40294b338
Ravit <dvirravi@gmail.com>
2022-04-25 17:34:02 +0300

    v2

--- recipes_and_more/src/components/MainPage.js
+++ recipes_and_more/src/components/MainPage.js
@@ -4,24 +4,55 @@ import { PageFrame } from './PageFrame';
 import 'bootstrap/dist/css/bootstrap.css';
 import Card from 'react-bootstrap/Card'
 import { Button, ListGroup } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel'
+import { SearchRecipe } from './SearchRecipe';
 
 export class MainPage extends React.Component {
 
 render() {
     return(
-    <>
+        <>
         <PageFrame /> 
-        {/* <img src="https://cdn.sallysbakingaddiction.com/wp-content/uploads/2018/08/homemade-strawberry-cupcakes-2.jpg.webp" class="img-fluid" alt="Background picture" height={100}></img>
-        <h1>Welcome to my recipe website</h1>
-        <figure class="text-center">
-            <blockquote class="blockquote">
-                <p>A well-known quote, contained in a blockquote element.</p>
-            </blockquote>
-            <figcaption class="blockquote-footer">
-                Someone famous in <cite title="Source Title">Source Title</cite>
-            </figcaption>
-        </figure>        */}
-    </>
+
+        <Carousel variant="dark">
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={require('./slide_1.jpg')}
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h5>TIP OF THE DAY:</h5>
+                <p>"To get crispy chocolate chip cookies, you should freeze the dough slightly before baking"</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={require('./slide_2.jpg')}
+                alt="Second slide"
+                />
+                <Carousel.Caption>
+                <h5>BEST MACARON RECIPE</h5>
+                <p>Coming Soon...</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={require('./slide_3.jpg')}
+                alt="Third slide"
+                />
+                <Carousel.Caption>
+                <h5>BAKERY OF THR WEEK:</h5>
+                <p>Lehamim Bakery [$$ - $$$]
+                    <br></br>
+                    Hashmonaim st. 103
+                </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            </Carousel>
+        </>
         )
     }
 }   
